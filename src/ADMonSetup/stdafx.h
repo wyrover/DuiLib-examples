3c9933d7c5c4d1a1d5c99af2ef6f9dad5867c51f
wyrover <wyrover@gmail.com>
2017-08-30 15:27:50 +0800

    + ADMonSetup

---
+++ src/ADMonSetup/stdafx.h
@@ -0,0 +1,28 @@
+// StdAfx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#if !defined(WIN32_LEAN_AND_MEAN)
+#define WIN32_LEAN_AND_MEAN	
+#endif
+
+#if !defined(_CRT_SECURE_NO_DEPRECATE)
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+// Windows Header Files:
+#include <windows.h>
+
+#include <tchar.h>
+
+#include "UIlib.h"
+
+using namespace DuiLib;
+
+
+
+// TODO: reference additional headers your program requires here
