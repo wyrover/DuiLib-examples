37059895e0c846875afd5b19b534bb2e5e4b439a
wyrover <wyrover@gmail.com>
2017-08-30 16:41:45 +0800

    + transwnd2

--- projects.lua
+++ projects.lua
@@ -194,4 +194,5 @@ workspace(path.getname(os.realpath(".")))
 
         create_duilib_project("HiDPITest", "src")
         create_duilib_project("menu_win_shadow_demo", "src")
+        create_duilib_project("transwnd2", "src")
         
---
+++ src/transwnd2/main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	::CoInitialize(NULL);
+    // 资源类型
+    CPaintManagerUI::SetResourceType(UILIB_FILE);
+    // 资源路径
+    CDuiString strResourcePath = CPaintManagerUI::GetInstancePath();    
+    strResourcePath += _T("..\\skin\\transwnd2\\");
+    CPaintManagerUI::SetResourcePath(strResourcePath.GetData());
+
+	CFrameWnd *pFrame = new CFrameWnd(_T("MainWnd.xml"));
+	pFrame->Create(NULL, _T("Redrain半透明窗体demo"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	pFrame->ShowModal();
+
+	delete pFrame;
+	::CoUninitialize();
+	return 0;
+}
